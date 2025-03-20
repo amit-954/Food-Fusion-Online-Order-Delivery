60859ffff6a7914383e936807847897a0b678eab
Amit Jana <101209091+amit-954@users.noreply.github.com>
2025-03-20 20:24:24 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -18,7 +18,12 @@ const Navbar = ({ setShowLogin }) => {
 
   return (
     <div className='navbar'>
-      <Link to='/'><img className='logo' src={assets.logo} alt="" /></Link>
+      <Link to='/'><img
+					className="logo"
+					src={assets.logo}
+					alt="Logo"
+					style={{ width: "220px", height: "auto", float: "left" }}
+				/></Link>
       <ul className="navbar-menu">
         <Link to="/" onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""}`}>home</Link>
         <a href='#explore-menu' onClick={() => setMenu("menu")} className={`${menu === "menu" ? "active" : ""}`}>menu</a>
