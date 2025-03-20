606b93fd1e51275d9a8a5b131b3e9da4d2e53b5f
Amit Jana <101209091+amit-954@users.noreply.github.com>
2025-03-20 23:13:21 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -35,14 +35,14 @@ const Footer = () => {
 				<div className="footer-content-right">
 					<h2>GET IN TOUCH</h2>
 					<ul>
-						<li>+1-212-456-7890</li>
-						<li>contact@tomato.com</li>
+						<li>+91-6297879966</li>
+						<li>contact@foodfusion.com</li>
 					</ul>
 				</div>
 			</div>
 			<hr />
 			<p className="footer-copyright">
-				Copyright 2024 © Tomato.com - All Right Reserved.
+				Copyright 2024 © Food Fusion - All Right Reserved.
 			</p>
 		</div>
   );
